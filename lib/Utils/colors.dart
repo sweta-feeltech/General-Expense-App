9035c70a64823f7f2ac517935c5b2f58896f1ec9
Sweta Panchal <sweta.feeltech@outlook.com>
2023-05-06 11:53:56 +0530

    6-5-23 colors added

--- lib/Utils/colors.dart
+++ lib/Utils/colors.dart
@@ -3,3 +3,5 @@ import 'dart:ui';
 const Color primaryPurple = Color.fromARGB(255 ,109, 129, 211);
 
 const Color primaryOrange = Color.fromARGB(255, 233, 136, 82);
+
+const Color primaryGrey = Color.fromARGB(255, 242, 242, 242);
