ca0e24ddadf945aae5a1c1af761700142c0c1508
FTS-04 <meet.feeltechsolution@outlook.com>
2023-05-06 12:45:55 +0530

    6-5-2023 : Bottom

--- lib/pages/Dashboard/add_inventory.dart
+++ lib/pages/Dashboard/add_inventory.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../Utils/colors.dart';
+
 
 class AddInventoryScreen extends StatefulWidget {
   const AddInventoryScreen({Key? key}) : super(key: key);
@@ -12,7 +14,8 @@ class _AddInventoryScreenState extends State<AddInventoryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: primaryGrey,
+
       appBar: AppBar(
         title: Text("Add Inventory"),
       ),
