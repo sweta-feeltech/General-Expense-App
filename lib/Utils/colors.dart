1ff63fded6bb9506b95a54e3a94932463aaeb1a5
Sweta Panchal <sweta.feeltech@outlook.com>
2023-05-08 16:46:47 +0530

    8-5-23  colors list logic for categories

--- lib/Utils/colors.dart
+++ lib/Utils/colors.dart
@@ -1,4 +1,6 @@
 import 'dart:ui';
+import 'package:flutter/material.dart';
+
 
 const Color primaryPurple = Color.fromARGB(255 ,109, 129, 211);
 
@@ -7,3 +9,24 @@ const Color primaryOrange = Color.fromARGB(255, 233, 136, 82);
 const Color primaryGrey = Color.fromARGB(255, 242, 242, 242);
 
 const Color darkGrey = Color.fromARGB(255, 78, 78, 78);
+
+
+List<Color> categoryBackColors = [
+  Color.fromARGB(255, 255, 237, 237),
+  Color.fromARGB(255, 229, 248, 254),
+  Color.fromARGB(255, 241, 239, 255),
+  Color.fromARGB(255, 238, 245, 227),
+  Color.fromARGB(255, 255, 255, 223)
+];
+
+Color getColor(int index, int lengthOfCourseList) {
+  if(index < categoryBackColors.length) {
+    return categoryBackColors[index];
+  }
+  else if(index >= categoryBackColors.length) {
+    return categoryBackColors[index % categoryBackColors.length];
+  }
+  else {
+    return Colors.red;
+  }
+}
